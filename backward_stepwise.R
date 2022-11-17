1e1dcd7534a83c1ae1f61bd356167cd8245167dd
Nam Nguyen <nguynh@iu.edu>
2022-11-17 10:01:30 -0500

    write the outline for R file

---
+++ backward_stepwise.R
@@ -0,0 +1,31 @@
+get_AIC <- function() {
+  
+}
+
+get_coef <- function() {
+  
+}
+
+get_F <- function() {
+  
+}
+
+get_sd <- function() {
+  
+}
+
+get_p <- function() {
+  
+}
+
+test <- function () {
+  
+}
+
+bws_selection <- function() {
+  get_AIC
+  get_coef
+  get_sd
+  get_F 
+  get_p
+}
