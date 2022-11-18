2c1e8d2a5a4004bd836c70dea0448a6b561561b6
Nam Nguyen <nguynh@iu.edu>
2022-11-17 22:15:46 -0500

    add computing functions + comments

--- backward_stepwise.R
+++ backward_stepwise.R
@@ -1,31 +1,51 @@
-get_AIC <- function() {
-  
+## Note:
+# Remove all rows containing missing data in your data set
+
+
+# Separate functions
+make_matrix_X <- function(...) cbind(1,...) # X is a matrix of predictors with the first column is a vector of ones
+get_beta <- function(X, y) solve(t(X)%*%X) %*% t(X) %*% y
+get_yhat <- function(X, beta) X %*% beta
+get_sigma <- function(yhat, y) sqrt(sum((y-yhat)^2)/(nrow(X)-ncol(X)))
+get_se <- function(X, sigma) sqrt(diag(sigma^2*solve(t(X)%*%X)))
+t_test <- function(beta, se, X) {
+  t_stat <- beta/se
+  p_value <- 2*pt(t_stat, df = nrow(X)-ncol(X), lower.tail=FALSE)
+  result <- cbind(t_stat, p_value)
+  colnames(result) <- c("t-stat", "p-value")
+  return(result)
 }
 
-get_coef <- function() {
-  
+get_AIC <- function(y, yhat, n, num_p) {
+  k <- 1 + num_p # including x-coefficient, the intercept, and sigma
+  logLik_2 <- n*(log(2*pi)+1+log((sum((y-yhat)^2)/n))) # -2log(L)
+  if (n/k >= 40) {
+    AIC <- 2*k + logLik_2
+  } else {
+    AIC <- 2*k + logLik_2 + (2*k^2+2*k)/(n-k-1)
+  }
+  return(AIC)
 }
 
-get_F <- function() {
-  
+# test function
+# inputs of this function are: y (response vector), x_i,...,x_p respectively.
+# Note that if you wanna check the result with lm function, you have to place x_i,...,x_p in the same order as a model in lm
+test_func <- function(y, ...) {
+  X <- make_matrix_X(...)
+  n <- nrow(X)
+  p <- ncol(X)
+  beta <- get_beta(X,y)
+  yhat <- get_yhat(X,beta)
+  AIC <- get_AIC(y, yhat, n, p)
+  return(AIC)
 }
 
-get_sd <- function() {
-  
-}
-
-get_p <- function() {
-  
-}
-
+# test function
 test <- function () {
   
 }
 
+# main function
 bws_selection <- function() {
-  get_AIC
-  get_coef
-  get_sd
-  get_F 
-  get_p
+  
 }
